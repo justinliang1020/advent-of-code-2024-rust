2c52d343b42e1cfb5abb5ef815d665c634168dd6
Justin Liang <justinliang1020@gmail.com>
2024-12-18 20:42:09 -0500

    part 1 day 1

--- src/days/day01/mod.rs
+++ src/days/day01/mod.rs
@@ -1,4 +1,29 @@
 use std::collections;
+use std::iter::zip;
+
+#[allow(dead_code)]
+fn part1(input: &str) -> u32 {
+    let mut a1: Vec<u32> = Vec::new();
+    let mut a2: Vec<u32> = Vec::new();
+    for line in input.lines() {
+        let mut line_split = line.split_whitespace();
+        a1.push(line_split.next().unwrap().parse().unwrap());
+        a2.push(line_split.next().unwrap().parse().unwrap());
+    }
+    a1.sort();
+    a2.sort();
+
+    let mut res: u32 = 0;
+    for iter in zip(a1, a2) {
+        if iter.0 > iter.1 {
+            res += iter.0 - iter.1;
+        } else {
+            res += iter.1 - iter.0;
+        }
+    }
+
+    res
+}
 
 #[allow(dead_code)]
 fn part2(input: &str) -> u32 {
@@ -28,6 +53,11 @@ mod tests {
     use super::*;
 
     const REAL_INPUT: &str = include_str!("input.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(REAL_INPUT), 1579939)
+    }
 
     #[test]
     fn test_part2() {
