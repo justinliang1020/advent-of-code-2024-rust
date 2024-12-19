30885e280b4df39275048587458481f087bc2cc7
Justin Liang <justinliang1020@gmail.com>
2024-12-19 16:49:13 -0500

    day 3 part 2

--- src/days/day03/mod.rs
+++ src/days/day03/mod.rs
@@ -13,8 +13,25 @@ fn part1(input: &str) -> u32 {
 
 #[allow(dead_code)]
 fn part2(input: &str) -> u32 {
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    0
+    let re = Regex::new(r"(mul\((?<x>\d{1,3}),(?<y>\d{1,3})\))|(?<do>do\(\))|(?<dont>don't\(\))")
+        .unwrap();
+    let mut sum: u32 = 0;
+    let caps = re.captures_iter(input);
+    let mut enabled = true;
+    for c in caps {
+        // c.get(0) returns the entire match
+        match c.get(0).unwrap().as_str() {
+            r"do()" => enabled = true,
+            r"don't()" => enabled = false,
+            _ => {
+                if enabled {
+                    // kinda unsafe?
+                    sum += c["x"].parse::<u32>().unwrap() * c["y"].parse::<u32>().unwrap();
+                }
+            }
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -23,6 +40,7 @@ mod tests {
 
     const REAL_INPUT: &str = include_str!("input.txt");
     const TEST_INPUT: &str = include_str!("test.txt");
+    const TEST2_INPUT: &str = include_str!("test2.txt");
 
     #[test]
     fn test_part1() {
@@ -31,7 +49,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 0)
+        assert_eq!(part2(TEST2_INPUT), 48)
     }
 
     #[test]
@@ -39,11 +57,10 @@ mod tests {
         println!("Part 1 Output: {}", part1(REAL_INPUT))
     }
 
-    // #[test]
-    // fn real_part2() {
-    //     println!("Part 2 Output: {}", part2(REAL_INPUT))
-    // }
-    //
+    #[test]
+    fn real_part2() {
+        println!("Part 2 Output: {}", part2(REAL_INPUT))
+    }
 
     #[test]
     fn real_test_part1() {
