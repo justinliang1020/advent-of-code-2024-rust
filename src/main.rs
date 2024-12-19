5ad7ef0678b096d4be61c66b291562a636e00bd1
Justin Liang <justinliang1020@gmail.com>
2024-12-18 19:20:33 -0500

    clippy and stuff

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,19 @@ fn main() {
 
     for line in fs::read_to_string(file_path).unwrap().lines() {
         let second_num: i32 = line.split_whitespace().nth(1).unwrap().parse().unwrap();
-        if map.contains_key(&second_num) {
-            map.insert(second_num, map.get(&second_num).unwrap() + 1);
-        } else {
-            map.insert(second_num, 1);
-        }
+        map.entry(second_num).and_modify(|e| *e += 1).or_insert(1);
     }
 
     let res = fs::read_to_string(file_path)
         .unwrap()
         .lines()
-        .map(|line| line.split(" ").nth(0).unwrap().parse::<i32>().unwrap())
+        .map(|line| {
+            line.split_whitespace()
+                .next()
+                .unwrap()
+                .parse::<i32>()
+                .unwrap()
+        })
         .fold(0, |acc, n| acc + n * map.get(&n).unwrap_or(&0));
 
     println!("{res}")
