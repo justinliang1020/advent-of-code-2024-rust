82eca350783783b4d6d14847574d7d0340398dd0
Justin Liang <justinliang1020@gmail.com>
2024-12-20 15:24:37 -0500

    day 05 part2

--- src/days/day05/mod.rs
+++ src/days/day05/mod.rs
@@ -1,9 +1,9 @@
 // template code for each dayXX/mod.rs file
 //
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-fn parse_input(input: &str) -> (HashSet<String>, Vec<&str>) {
+fn parse_input_set(input: &str) -> (HashSet<(&str, &str)>, Vec<&str>) {
     let mut set = HashSet::new();
 
     let split: Vec<&str> = input.split("\n\n").collect();
@@ -11,7 +11,8 @@ fn parse_input(input: &str) -> (HashSet<String>, Vec<&str>) {
 
     for s in s1.split_whitespace() {
         // format i.e. 12|86
-        set.insert(s.to_string());
+        let split = s.split("|").collect::<Vec<&str>>();
+        set.insert((split[0], split[1]));
     }
     let lines: Vec<&str> = s2.split_whitespace().collect();
 
@@ -20,20 +21,19 @@ fn parse_input(input: &str) -> (HashSet<String>, Vec<&str>) {
 
 #[allow(dead_code)]
 fn part1(input: &str) -> u32 {
-    let (set, lines) = parse_input(input);
+    let (set, lines) = parse_input_set(input);
     let mut res = 0;
 
     for line in lines {
-        let l: Vec<&str> = line.split(",").collect();
+        let line: Vec<&str> = line.split(",").collect();
 
         let mut correct = true;
-        for i in 0..l.len() {
+        for i in 0..line.len() {
             if !correct {
                 break;
             }
-            for j in i + 1..l.len() {
-                let entry = format!("{}|{}", l[i], l[j]);
-                if !set.contains(&entry) {
+            for j in i + 1..line.len() {
+                if !set.contains(&(line[i], line[j])) {
                     correct = false;
                     break;
                 }
@@ -41,7 +41,7 @@ fn part1(input: &str) -> u32 {
         }
 
         if correct {
-            res += l[l.len() / 2].parse::<u32>().unwrap();
+            res += line[line.len() / 2].parse::<u32>().unwrap();
         }
     }
 
@@ -50,7 +50,51 @@ fn part1(input: &str) -> u32 {
 
 #[allow(dead_code)]
 fn part2(input: &str) -> u32 {
-    0
+    // create map of frequency for each correct line
+    // then generate order by getting most element wiht highest number of overlapping values in the
+    // map
+    // whatever
+    let (set, lines) = parse_input_set(input);
+    let mut res = 0;
+
+    for line in lines {
+        let line: Vec<&str> = line.split(",").collect();
+
+        let mut correct = true;
+        for i in 0..line.len() {
+            if !correct {
+                break;
+            }
+            for j in i + 1..line.len() {
+                if !set.contains(&(line[i], line[j])) {
+                    correct = false;
+                    break;
+                }
+            }
+        }
+
+        if !correct {
+            // do stuff
+            let mut freqs: Vec<(&str, usize)> = vec![];
+
+            for i in 0..line.len() {
+                let mut line_set: HashSet<(&str, &str)> = HashSet::new();
+                for j in 0..line.len() {
+                    if i == j {
+                        continue;
+                    }
+                    line_set.insert((line[i], line[j]));
+                }
+                let intersection_count = line_set.intersection(&set).count();
+                freqs.push((line[i], intersection_count))
+            }
+
+            freqs.sort_by_key(|k| k.1);
+            res += freqs[freqs.len() / 2].0.parse::<u32>().unwrap();
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -67,7 +111,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 0)
+        assert_eq!(part2(TEST_INPUT), 123)
     }
 
     #[test]
@@ -75,8 +119,8 @@ mod tests {
         println!("Part 1 Output: {}", part1(REAL_INPUT))
     }
 
-    // #[test]
-    // fn real_part2() {
-    //     println!("Part 2 Output: {}", part2(REAL_INPUT))
-    // }
+    #[test]
+    fn real_part2() {
+        println!("Part 2 Output: {}", part2(REAL_INPUT))
+    }
 }
